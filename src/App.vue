73cbc1bb52ad3f2f6e2c6e697bcde9f64bb4e6ca
NashibJr <nashibjr@gmail.com>
2023-08-31 04:51:47 -0700

    I have added the input field

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,44 @@
+<script setup>
+  import { reactive, watch } from 'vue';
+
+  const state = reactive({
+    todo: '',
+    todos: []
+  });
+  
+</script>
+
 <template>
   <div>
     <header>
       <Header />
     </header>
+    <main>
+      <form>
+        <input type="text"
+          v-model="state.todo"
+          placeholder="Entet task..."
+          class="input-content"
+        />
+      </form>
+    </main>
   </div>
 </template>
 
+<style scoped>
+  main {
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    padding: 10px;
+  }
+  .input-content {
+    margin: 5px;
+    padding: 7px;
+    width: 92%;
+    font-family: century gothic;
+    font-size: 1em;
+    border: 1px solid rgba(0, 0, 0, .3);
+    outline: none;
+  }
+</style>
