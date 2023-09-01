0a90d14ea984e6a87d7c05d3b044d4d01b28c2ff
NashibJr <nashibjr@gmail.com>
2023-08-31 23:31:59 -0700

    I have added a dummy todo task

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <script setup>
-  import { reactive, watch } from 'vue';
+  import { reactive } from 'vue';
 
   const state = reactive({
     todo: '',
-    todos: []
+    todos: [
+      { id: 1, body: 'Eating' },
+      { id: 2, body: 'Showering' },
+      { id: 3, body: 'sleeping' },
+    ],
+    completed: [],
   });
   
 </script>
@@ -20,10 +25,30 @@
           placeholder="Entet task..."
           class="input-content"
         />
+        <span :style="{display: 'flex', justifyContent: 'center'}">
+          <button type="button" class="button-content">add</button>
+        </span>
       </form>
-      <span :style="{display: 'flex', justifyContent: 'center'}">
-        <button type="button" class="button-content">add</button>
-      </span>
+      <div class="todos">
+        <ul>
+          <li>
+            <span>
+              <input 
+                type="checkbox" 
+                id="completed" 
+                value="completed" 
+                v-model="state.completed" 
+              />
+              Eating
+            </span>
+            <v-icon 
+              name="ri-delete-bin-line" 
+              scale="1.3" 
+              class="icon"
+            />
+          </li>
+        </ul>
+      </div>
     </main>
   </div>
 </template>
@@ -61,5 +86,43 @@
   }
   .button-content:hover {
     opacity: .8;
+  }
+
+  .todos ul {
+    display: flex;
+    width: 100%;
+    margin: 5px;
+    padding: 5px;
+    flex-direction: column;
+    flex-wrap: wrap;
+    list-style-type: none;
+  }
+
+  .todos ul li {
+    display: flex;
+    justify-content: space-between;
+    margin: 5px;
+    padding: 5px;
+    border: none;
+    border-bottom: 1px solid rgba(0, 0, 0, .3);
+  }
+
+  .todos ul li span {
+    width: 90%;
+    display: flex;
+    flex-wrap: wrap;
+    padding-top: 2px;
+  }
+
+  .todos ul li span input {
+    margin-right: 10px;
+    width: 17px;
+    height: 17px;
+    transform: translateY(-1px);
+  }
+
+  .icon:hover {
+    color: red;
+    cursor: pointer;
   }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import Header from "./components/Header.vue";
+import { RiDeleteBinLine } from "oh-vue-icons/icons";
+import { OhVueIcon, addIcons } from "oh-vue-icons";
+
+addIcons(RiDeleteBinLine);
 
 const app = createApp(App);
-app.component("Header", Header).mount("#app");
+app.component("Header", Header).component("v-icon", OhVueIcon);
+app.mount("#app");
